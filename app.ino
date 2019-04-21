e43d78dabe329d52e3173350e4ac7b6d33398ded
David-M-Nielsen <monbergnielsen@outlook.com>
2019-04-21 23:08:24 +0200

    finished project

--- app.ino
+++ app.ino
@@ -1,53 +1,114 @@
 #include <stdint.h>
 
-#define RED_LEFT 2
-#define YELLOW_LEFT 3
-#define GREEN_LEFT 4
+const uint8_t LEFT_PINS[3] = {2, 3 ,4};
+const uint8_t RIGHT_PINS[3] = {8, 9, 10};
+const uint16_t interval = 1000;
+const uint8_t R = 0;
+const uint8_t Ry = 1;
+const uint8_t Y = 2;
+const uint8_t G = 3;
 
-#define RED_RIGHT 8
-#define YELLOW_RIGHT 9
-#define GREEN_RIGHT 10
-
-/*
-	struct state options:
-	0: red
-	1: red + yellow
-	2: yellow
-	3: green
-*/
-
-typedef struct Light {
-	uint8_t state;
-	bool active;
+typedef struct {
+  uint8_t state;
+  bool goGreen;
+  uint8_t pins[3];
 } light;
 
-void nextState(light input){
-	Serial.printf(input.state);
-}
+int16_t timer;
+uint16_t lastChange;
 
-uint16_t stamp;
-
-void ledOn(int led) {
-	digitalWrite(led, HIGH);
-}
-void ledOff(int led) {
-	digitalWrite(led, LOW);
-}
+light leftLight;
+light rightLight;
 
 void setup() {
-	Serial.begin(9600);
+  Serial.begin(9600);
 
-	stamp = millis();
+  pinMode(LEFT_PINS[0], OUTPUT);
+  pinMode(LEFT_PINS[1], OUTPUT);
+  pinMode(LEFT_PINS[2], OUTPUT);
+  pinMode(RIGHT_PINS[8], OUTPUT);
+  pinMode(RIGHT_PINS[9], OUTPUT);
+  pinMode(RIGHT_PINS[10], OUTPUT);
 
-	pinMode(RED_LEFT, OUTPUT);
-	pinMode(YELLOW_LEFT, OUTPUT);
-	pinMode(GREEN_LEFT, OUTPUT);
+  lastChange = 0;
+  
+  leftLight = {
+    G,
+    false,
+    {LEFT_PINS[0],LEFT_PINS[1],LEFT_PINS[2]}
+  };
 
-	pinMode(RED_RIGHT, OUTPUT);
-	pinMode(YELLOW_RIGHT, OUTPUT);
-	pinMode(GREEN_RIGHT, OUTPUT);
+  rightLight = {
+    R,
+    true,
+    {RIGHT_PINS[0],RIGHT_PINS[1],RIGHT_PINS[2]}
+  };
+
 }
 
 void loop(){
+  timer = millis();
 
+  if(timer - lastChange >= interval){
+    nextState(&leftLight);
+    nextState(&rightLight);
+
+    handleState(&leftLight);
+    handleState(&rightLight);
+    
+    lastChange = timer;
+  }
+}
+
+void ledOn(uint8_t led) {
+  digitalWrite(led, HIGH);
+}
+
+void ledOff(uint8_t led) {
+  digitalWrite(led, LOW);
+}
+
+void nextState(light* l){
+  if((l->state == R && l->goGreen == false) || (l->state == G && l->goGreen == true)){
+    l->goGreen = !l->goGreen;
+  }
+
+  if(l->goGreen){
+    if(l->state == Ry){
+      l->state = G;
+    } else {
+      l->state = Ry;
+    }
+  } else {
+    if(l->state == Y){
+      l->state = R;
+    } else {
+      l->state = Y;
+    }
+  }
+}
+
+void handleState(light* l){
+  switch (l->state) {
+    case R:
+      ledOn(l->pins[0]);
+      ledOff(l->pins[1]);
+      ledOff(l->pins[2]);
+      break;
+    case Ry:
+      ledOn(l->pins[0]);
+      ledOn(l->pins[1]);
+      ledOff(l->pins[2]);
+      break;
+    case Y:
+      ledOff(l->pins[0]);
+      ledOn(l->pins[1]);
+      ledOff(l->pins[2]);
+      break;
+    case G:
+      ledOff(l->pins[0]);
+      ledOff(l->pins[1]);
+      ledOn(l->pins[2]);
+      break;
+  }
 }
